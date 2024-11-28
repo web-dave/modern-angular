1db0145b635135dcfd33f774e53d65be0db66758
Dave <dave@webdave.de>
2024-11-28 10:23:50 +0100

    Fix missing import of MainNavigation

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { BookApiService } from '../book-api.service';
-import { Book } from '../models';
-import { NgFor, NgIf, AsyncPipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { BookCardComponent } from '../book-card/book-card.component';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { BookApiService } from "../book-api.service";
+import { Book } from "../models";
+import { NgFor, NgIf, AsyncPipe } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { BookCardComponent } from "../book-card/book-card.component";
 
 @Component({
-    selector: 'ws-book-list',
-    templateUrl: 'book-list.component.html',
-    imports: [NgFor, NgIf, RouterLink, BookCardComponent, AsyncPipe]
+  selector: "ws-book-list",
+  templateUrl: "book-list.component.html",
+  imports: [NgFor, NgIf, RouterLink, BookCardComponent, AsyncPipe],
 })
 export class BookListComponent {
   books$: Observable<Book[]>;
