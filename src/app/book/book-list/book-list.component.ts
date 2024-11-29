1d0019a7885cc82c3390ae9c2f0de2a9fc78645d
Dave <dave@webdave.de>
2024-11-29 12:16:45 +0100

    @defer

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -4,11 +4,12 @@ import { RouterLink } from "@angular/router";
 import { BookCardComponent } from "../book-card/book-card.component";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { BookDetailComponent } from "../book-detail/book-detail.component";
+import { BookFooterComponent } from "./footer.component";
 
 @Component({
   selector: "ws-book-list",
   templateUrl: "book-list.component.html",
-  imports: [RouterLink, BookCardComponent, BookDetailComponent],
+  imports: [RouterLink, BookCardComponent, BookFooterComponent],
 })
 export class BookListComponent {
   books = toSignal(inject(BookApiService).getAll(), { initialValue: [] });
---
+++ src/app/book/book-list/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "ws-book-footer",
+  template: `<h1>Footer</h1>`,
+  standalone: true,
+  imports: [],
+})
+export class BookFooterComponent {}
